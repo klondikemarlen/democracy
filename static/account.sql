e6ba35751eb51efa2635df5b5fbc9db543c021b6
Talesh Seeparsan <talesh@seeparsan.net>
2018-11-16 19:09:39 -0800

    adding the import sql files

---
+++ static/account.sql
@@ -0,0 +1,7 @@
+INSERT INTO account (id,username,email,password_hash) VALUES (1,"adesola","adesola@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (2,"marlen","marlen@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (3,"jutta","jutta@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (4,"lue","lue@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (5,"talesh","talesh@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (6,"greg","greg@email.com","password");
+INSERT INTO account (id,username,email,password_hash) VALUES (7,"alser","alser@email.com","password");
---
+++ static/question.sql
@@ -0,0 +1,14 @@
+INSERT INTO question(id,text,issue_id) VALUES (1,"Who should run the new homless shelter?",1);
+INSERT INTO question(id,text,issue_id) VALUES (2,"What services should a homeless provide?",1);
+INSERT INTO question(id,text,issue_id) VALUES (3,"Who should pay for the the new homless shelter?",1);
+INSERT INTO question(id,text,issue_id) VALUES (4,"What are the barriers to building the new homeless shelter?",1);
+INSERT INTO question(id,text,issue_id) VALUES (5,"Is there a need for a new homeless shelter?",1);
+INSERT INTO question(id,text,issue_id) VALUES (6,"Will fracking cause earthquakes?",2);
+INSERT INTO question(id,text,issue_id) VALUES (7,"What will be done with fracking waste?",2);
+INSERT INTO question(id,text,issue_id) VALUES (8,"Will fracking pollute groundwater?",2);
+INSERT INTO question(id,text,issue_id) VALUES (9,"What impact will there be on wildlife?",2);
+INSERT INTO question(id,text,issue_id) VALUES (10,"How will fracking affecte Yukon communities?",2);
+INSERT INTO question(id,text,issue_id) VALUES (11,"Where can I use cannabis?",3);
+INSERT INTO question(id,text,issue_id) VALUES (12,"Can I grow pot?",3);
+INSERT INTO question(id,text,issue_id) VALUES (13,"How much pot can I have?",3);
+INSERT INTO question(id,text,issue_id) VALUES (14,"Is my medical use card still valid",3);
