d7ded6fa95166d9dd4d4da84d5d5b49a27887312
Marlen Brunner <klondikemarlen@gmail.com>
2018-11-16 18:11:49 -0800

    Fix login, broken post.

--- tenacity/route/login.py
+++ tenacity/route/login.py
@@ -6,9 +6,11 @@ from tenacity.service import login as login_service
 
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
-
-    form_data = flask.json.loads(next(flask.request.form.keys()))
     if flask.request.method == 'POST':
+        try:
+            form_data = flask.json.loads(next(flask.request.form.keys()))
+        except StopIteration:
+            return flask.jsonify(login=False)
         if login_service.login_account(form_data['username'], form_data['password']):
             return flask.jsonify(login=True)
         return flask.jsonify(login=False)
